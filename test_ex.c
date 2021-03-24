3a54f4e3b2d72f15ba5d403534d8c9fa6f129671
TheSchilk <schilk.philipp@gmail.com>
2021-03-24 10:36:22 +0100

    First proof of concept

---
+++ test_ex.c
@@ -0,0 +1,9 @@
+//Include the right header to be able to use printf:
+#include ____
+
+
+int main(int argc, char* argv[]){
+    //Complete the following printf to say "Hello World!" 
+    printf(___);
+    
+}
---
+++ test_sol.c
@@ -0,0 +1,19 @@
+//$ START EX 
+// //Include the right header to be able to use printf:
+// #include ____
+//$ END
+
+//$ START SOL
+#include <stdio.h>
+//$ END
+
+int main(int argc, char* argv[]){
+    //$ START EX
+    // //Complete the following printf to say "Hello World!" 
+    // printf(___);
+    //$ END
+    
+    //$ START SOL 
+    printf("Hello World!\n");
+    //$ END
+}
