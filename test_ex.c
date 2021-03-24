3625fd513d3c54fb1da47321895de2e3c289f78d
TheSchilk <schilk.philipp@gmail.com>
2021-03-24 10:45:03 +0100

    Actual cleanup

--- test_ex.c
+++
@@ -1,9 +0,0 @@
-//Include the right header to be able to use printf:
-#include ____
-
-
-int main(int argc, char* argv[]){
-    //Complete the following printf to say "Hello World!" 
-    printf(___);
-    
-}
